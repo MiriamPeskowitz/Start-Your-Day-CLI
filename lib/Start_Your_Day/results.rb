0d940f82286ebdacdc952a838c3cf27aab2335aa
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-16 16:15:57 +0000

    Done.

--- lib/Start_Your_Day/results.rb
+++ lib/Start_Your_Day/results.rb
@@ -1,8 +1,14 @@
 class Start_Your_Day::results
-
+@@all = {}
   #what am i creating: a collection of the scrape results
   def self.today
+    self.starters
     #4 things, ready to puts on the controller
+  end
+
+
+  def self.all
+    doc1 =
   end
 
 end
--- lib/starters_scrape.rb
+++ lib/starters_scrape.rb
@@ -1,22 +1,27 @@
 require 'pry'
-require 'Nokogiri'
+require 'nokogiri'
 
 class Start_Your_Day::Scraper
 
+  def self.today
+    self.scrape_all_todays_starters
+  end
 
+  def self.scrape_all_todays_starters
+    starters = []
+    starters << self.scrape_meditation,self.scrape_writing_tip, self.scrape_word_of_the_day, self.scrape_five_ideas
+  end
 
   def self.scrape_meditation
-<<<<<<< HEAD
     doc1 = Nokogiri::HTML(open("https://www.dailyzen.com/"))
-=======
-    binding.pry
-    doc1 = Nokogiri::HTML(open("https://www.dailyzen.com/"))
-    meditation = cod.search()
->>>>>>> eb40152a6e9f407b45b74397f9283713a6e2a31f
+    meditation = doc1.css("blockquote")
+    # div above it is .zen-quote
   end
 
   def self.scrape_writing_tip
     doc2 = Nokogiri::HTML(open("https://www.dailywritingtips.com/"))
+    doc2.css(".post-block h2 a href").first-child of post-block   #this is the title and link # => how do i reference the first post block ??
+    docs2.css(".post-block p").first-child of post-block# this is the description
   end
 
   def self.scrape_word_of_the_day
@@ -28,7 +33,3 @@ class Start_Your_Day::Scraper
   end
 
 end
-<<<<<<< HEAD
-=======
-  
->>>>>>> eb40152a6e9f407b45b74397f9283713a6e2a31f
