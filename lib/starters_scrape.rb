f75f6e9af108ef0663cec14f106cc7e0096168a3
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-16 15:04:40 +0000

    changed instance methods to class methods

--- lib/starters_scrape.rb
+++ lib/starters_scrape.rb
@@ -1,18 +1,18 @@
 class Start_Your_Day::CLI
 
-  def scrape_meditation
+  def self.scrape_meditation
     https://www.dailyzen.com/
   end
 
-  def scrape_writing_tip
+  def self.scrape_writing_tip
     https://www.dailywritingtips.com/
   end
 
-  def scrape_word_of_the_day
+  def self.scrape_word_of_the_day
     https://www.merriam-webster.com/word-of-the-day
   end
 
-  def scrape_five_ideas
+  def self.scrape_five_ideas
     https://www.aspeninstitute.org/ideas/
   end
 
