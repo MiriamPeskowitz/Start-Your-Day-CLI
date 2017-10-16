a643055f2dc39a4c1bd08eb349875b17b2d4bce6
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-16 15:03:17 +0000

    created the object and four instance methods

---
+++ lib/starters_scrape.rb
@@ -0,0 +1,19 @@
+class Start_Your_Day::CLI
+
+  def scrape_meditation
+    https://www.dailyzen.com/
+  end
+
+  def scrape_writing_tip
+    https://www.dailywritingtips.com/
+  end
+
+  def scrape_word_of_the_day
+    https://www.merriam-webster.com/word-of-the-day
+  end
+
+  def scrape_five_ideas
+    https://www.aspeninstitute.org/ideas/
+  end
+
+end
